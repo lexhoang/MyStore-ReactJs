43fad46a648439c3c91467b6ba1c0256393039d2
LeHoang <lehoang999113@gmail.com>
2022-09-03 15:51:25 +0700

    heroku

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import firebase from "firebase";
 import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyArqJO9fuI2QQ0LjHwS7v2Ot9BXlP0MJxM",
-    authDomain: "vape-store-999.firebaseapp.com",
-    projectId: "vape-store-999",
-    storageBucket: "vape-store-999.appspot.com",
-    messagingSenderId: "540576607364",
-    appId: "1:540576607364:web:2d82946bb14ffb9908e6d1",
-    measurementId: "G-PD9NC3PKMY"
+    apiKey: "AIzaSyDjfgaEiWX_AXfYrB_lR_lJH2ICLSPqndI",
+    authDomain: "watch-store-999.firebaseapp.com",
+    projectId: "watch-store-999",
+    storageBucket: "watch-store-999.appspot.com",
+    messagingSenderId: "839341194585",
+    appId: "1:839341194585:web:2c2b9bd78d4f444640e048",
+    measurementId: "G-B15RMEG7KQ"
 };
 
 firebase.initializeApp(firebaseConfig);
